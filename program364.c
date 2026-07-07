ae1f2dc27ab1f7683a915a3b28fd01084d0bec25
Rajveer701 <rajveerrajput179@gmail.com>
2026-07-07 21:44:06 +0530

    Initial commit

---
+++ program364.c
@@ -0,0 +1,27 @@
+//malloc and pointer
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+
+struct node{
+    int data;
+    struct node *next;
+
+};
+
+typedef struct node NODE; 
+typedef struct node * PNODE; 
+
+int main(){
+    struct node * newn = NULL;
+
+    newn = (struct node *)malloc(sizeof(struct node));   //dynamic memory allocation
+
+    newn -> data = 11;
+    newn -> next = NULL;
+
+    printf("%d\n",newn->data);
+
+    return 0;
+}
